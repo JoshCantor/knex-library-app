371001e0a2f974fc8dfefcc0c194a1962d5a6153
JoshCantor <josh.s.cantor@gmail.com>
2016-01-09 09:29:17 -0800

    can add authors to database and then redirect to list of authors

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,13 +1,24 @@
 var express = require('express'),
-	router = express.Router();
+	router = express.Router(),
+	knex = require('../db/knex');
 
 router.get('/', function(req, res) {
-	res.render('../views/authors/index');
+	knex('authors').then(function(authors) {
+		console.log('auth', authors);
+		res.render('../views/authors/index', {authors: authors});
+	});
 });	
 
 router.get('/new', function(req, res) {
 	res.render('../views/authors/new');
 });
+
+router.post('/new', function(req, res) {
+	var name = req.body.name;
+	knex('authors').insert({name: name}).then(function(result) {
+		res.redirect('/authors');
+	});
+})
 
 router.get('/update', function(req, res) {
 	res.render('../views/authors/update');
--- library.js
+++ library.js
@@ -1,11 +1,15 @@
 var express = require('express'),
 	app = express(),
-	authors = require('./controllers/authors');
+	authors = require('./controllers/authors'),
+	bodyParser = require('body-parser'),
+	morgan = require('morgan');
+require('locus');
 
 app.set('view engine', 'ejs');
-
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/authors', authors);
-
 
 
 app.listen(3000, function() {
